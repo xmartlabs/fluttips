de831563e1b9bc0890f566915563c6661d722484
Sol Mendiola <solmendiola73@gmail.com>
2022-11-04 10:58:22 -0300

    Resolve problems with format checks

--- lib/ui/home/drawer.dart
+++ lib/ui/home/drawer.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_template/ui/extensions/context_extensions.dart';
