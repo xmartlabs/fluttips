b3f60ef669eb6f90a4cdf495409f2ebab631d0c3
mirland <mirland@xmartlabs.com>
2022-11-21 15:28:48 -0300

    Include Google Service Json

--- scripts/firebase_setup.sh
+++ scripts/firebase_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp $1/google-services.json android/app/
-mkdir -p ios/Runner/Resources/Fireabse
-cp $1/GoogleService-Info-dev.plist ios/Runner/Resources/Fireabse/
-cp $1/GoogleService-Info-prod.plist ios/Runner/Resources/Fireabse/
+mkdir -p ios/Runner/Resources/Firebase
+cp $1/GoogleService-Info-dev.plist ios/Runner/Resources/Firebase/
+cp $1/GoogleService-Info-prod.plist ios/Runner/Resources/Firebase/
