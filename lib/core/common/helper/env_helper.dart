f541163696fd3ba12c625fe98ad954258aea5588
Sol Mendiola <57235692+SolMendiola@users.noreply.github.com>
2022-11-25 14:04:07 -0300

    Added bugsee (#34)
    * Added bugsee
    
    * Added bugsee
    
    * Xcode changes
    
    * Minor change
    
    * Resolve last comments
    
    * Fix some issues
    
    * resolve comments
    
    Co-authored-by: mirland <mirland@xmartlabs.com>

--- lib/core/common/helper/env_helper.dart
+++ lib/core/common/helper/env_helper.dart
@@ -4,9 +4,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fluttips/core/common/logger.dart';
 
 Future<Map<String, String>> loadEnvs(
-  String path, [
+  String path, {
   bool ignoreErrors = true,
-]) async {
+}) async {
   final dotEnv = DotEnv();
   final completer = Completer<Map<String, String>>();
   // ignore: unawaited_futures
