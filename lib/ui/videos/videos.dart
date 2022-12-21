d8c77b7623cc9fd029976a36ef2222e21f9a02fe
Sol Mendiola <solmendiola73@gmail.com>
2022-12-21 13:59:18 -0300

    Changed empty favourite and videos view

--- lib/ui/videos/videos.dart
+++ lib/ui/videos/videos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttips/ui/common/context_extensions.dart';
 import 'package:fluttips/ui/theme/app_theme.dart';
 
@@ -12,10 +13,24 @@ class VideosScreen extends StatelessWidget {
 class _VideosContentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Center(
-        child: Text(
-          'VIDEOS',
-          style: context.theme.textStyles.headlineMedium!
-              .copyWith(color: context.theme.colors.onPrimary),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              context.localizations.videos,
+              style: context.theme.textStyles.bodyMedium!.copyWith(
+                color: context.theme.colors.surface.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            Icon(
+              Icons.play_circle_outline_outlined,
+              color: context.theme.colors.surface.shade700,
+            ),
+          ],
         ),
       );
 }
