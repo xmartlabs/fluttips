b09981a62ae0954369be6ee95279be1cc90f92ec
Sol Mendiola <57235692+SolMendiola@users.noreply.github.com>
2023-01-18 17:23:44 -0300

    feat: Added clean data (#68)
    * Added clean data
    
    * Resolve comments
    
    * Resolve comments

--- lib/core/repository/session_repository.dart
+++ lib/core/repository/session_repository.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
-import 'package:fluttips/core/di/di_provider.dart';
 import 'package:fluttips/core/model/onboarding_status.dart';
 import 'package:fluttips/core/source/local_source/session_local_source.dart';
 
 class SessionRepository {
-  final SessionLocalSource _sessionLocalSource = DiProvider.get();
+  final SessionLocalSource _sessionLocalSource;
+
+  SessionRepository(this._sessionLocalSource);
 
   Stream<AppSessionStatus> getSessionStatus() => _sessionLocalSource
       .getSessionStatus()
